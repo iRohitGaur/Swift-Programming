586f5506fa51d8cfcc26ee1907ef143f038aeb49
Rohit Gaur <rohitgaur7@gmail.com>
2021-09-05 17:25:29 +0530

    add String Interpolation

---
+++ 3 - String Interpolation/3.swift
@@ -0,0 +1,7 @@
+let name = "Steve"
+print("Hello \(name)")
+// Hello Steve
+
+let celsius = 30
+print("Temperature in Celsius: \(celsius) and Fahrenheit: \( (celsius * 9/5) + 32 )")
+// Temperature in Celsius: 30 and Fahrenheit: 86
---
+++ 3 - String Interpolation/README.md
@@ -0,0 +1,2 @@
+![String Interpolation](3-1.gif)
+![String Interpolation](3-2.gif)
